48b251fa32de657e9ec644b75b6699c32a2131aa
lyndaflower <umurerwalynda@gmail.com>
2019-10-10 13:09:31 +0200

    add android practice

--- app/src/main/java/com/example/myfirst/MainActivity.java
+++ app/src/main/java/com/example/myfirst/MainActivity.java
@@ -2,13 +2,43 @@ package com.example.myfirst;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+import static android.icu.lang.UCharacter.GraphemeClusterBreak.V;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+      @BindView(R.id.findButton) Button mFindChallenger;
+      @BindView(R.id.appNameTextView) TextView mTextView;
+      @BindView(R.id.locationEditText) EditText mLocationEditText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+
+        mFindChallenger.setOnClickListener(this);
     }
+            @Override
+            public void onClick(View v) {
+
+                if(v == mFindChallenger) {
+                    String location = mLocationEditText.getText().toString();
+                    Intent intent = new Intent(MainActivity.this, AppActivity.class);
+                    intent.putExtra("location", location);
+                    startActivity(intent);
+                }
+
+            }
 }
---
+++ app/src/main/java/com/example/myfirst/MyAppArrayAdapter.java
@@ -0,0 +1,31 @@
+package com.example.myfirst;
+
+import android.content.Context;
+import android.widget.ArrayAdapter;
+
+public class MyAppArrayAdapter extends ArrayAdapter {
+    private Context mContext;
+    private String[] mRestaurants;
+    private String[] mCuisines;
+
+    public MyAppArrayAdapter(Context mContext, int resource, String[] mRestaurants, String[] mCuisines) {
+        super(mContext, resource);
+        this.mContext = mContext;
+        this.mRestaurants = mRestaurants;
+        this.mCuisines = mCuisines;
+    }
+
+    @Override
+    public Object getItem(int position) {
+        String restaurant = mRestaurants[position];
+        String cuisine = mCuisines[position];
+        return String.format("%S \nServes great : %S", restaurant, cuisine);
+
+    }
+
+    @Override
+    public int getCount(){
+        return  mRestaurants.length;
+    }
+}
+
